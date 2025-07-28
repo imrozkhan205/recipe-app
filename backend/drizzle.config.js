b26b46c7ba0f4d6d86f373a68a9b215360b849f6
imrozkhan205 <imrozkhan2258@gmail.com>
2025-07-28 20:28:50 +0530

    Initial commit

---
+++ backend/drizzle.config.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv'
+dotenv.config()
+export default {
+    schema: "./src/db/schema.js",
+    out: "./src/db/migrations",
+    dialect: 'postgresql',
+    dbCredentials: {url : process.env.DATABASE_URL},
+};
